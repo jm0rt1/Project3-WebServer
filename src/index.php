53398098bd17383cd815fb9293f56abbf77f0872
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-28 16:10:59 -0400

    removed duplicates

--- src/index.php
+++ src/index.php
@@ -86,45 +86,5 @@ $app->get('/api/message_recipients', function(Request $request, Response $respon
 		echo '{"error":{"text":'.$e->getMessage().'}';
 	}
 });
-$app->delete('/api/messages/delete', function (Request $request, Response $response)
-{
-	$id = $request->getParam('id');
-	$sql_query="DELETE FROM messages where id = '$id'";
-	try
-	{
-		$datab=connect_to_db();
-		$stmt=$datab->prepare($sql_query);
-		$stmt->execute();
-		$datab=null;
-		echo '{"Result:{"text":"User Deleted"}';
-	}
-	catch (PDOException $e)
-	{
-		echo '{"error":{"text":'.$e->getMessage().'}';
-	}
-});
-$app->post('/api/messages/add', function (Request $request, Response $response)
-{
-	$message_body = $request->getParam('message_body');
-	$sender_id = $request->getParam('sender_id');
-	$parent_message_id = $request->getParam('parent_message_id')
-
-	$sql_query="INSERT INTO messages (message_body,sender_id,parent_message_id) VALUES (:message_body,:sender_id,:parent_message_id)";
-	try
-	{
-		$datab=connect_to_db();
-		$stmt=$datab->prepare($sql_query);
-		$stmt->bindParam(':title',$title);
-		$stmt->bindParam(':content',$content);
-		$stmt->bindParam(':owner_id',$owner_id);
-		$stmt->execute();
-		$datab=null;
-		echo '{"Result:{"text":"Post Added"}';
-	}
-	catch (PDOException $e)
-	{
-		echo '{"error":{"text":'.$e->getMessage().'}';
-	}
-});
 
 $app->run();
