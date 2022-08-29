d4670bb68969380e028f1c4a10f2e3aeae5f9a84
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-29 19:38:43 -0400

    test more

--- src/index.php
+++ src/index.php
@@ -101,7 +101,7 @@ $app->get('/api/users[/{name}]', function(Request $request, Response $response, 
 	if(isset($args['name'])){
 		
 		$name = $args['name'];
-		$sql_query="SELECT * FROM users where \user_name = $name";
+		$sql_query="SELECT * FROM users where 'user_name' = $name";
 		echo $name;
 
 	}else{
